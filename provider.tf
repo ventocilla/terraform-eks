c08ffda60152bce9ca10ff5af58b411169b07a89
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-11-27 23:40:01 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.77.0"
     }
+  }
+  backend "s3" {
+    bucket = "zencoder-test-terraform-tfstate"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
