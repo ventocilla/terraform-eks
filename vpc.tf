38ef405620c86cb9fd5cd70a77f0f8b8d12ef7bf
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-12-02 11:05:08 -0300

    feat: decouple tags, cidrsubnet function

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,13 @@
 resource "aws_vpc" "eks_vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = {
-    Name = "eks_vpc"
-  }
+  tags = merge(
+    local.tags,
+    {
+      Name = "eks_vpc"
+    }
+  )
 
 }
